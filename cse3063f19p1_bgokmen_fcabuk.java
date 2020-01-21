6372c16397e6dc2c8faba90d9004854e03913298
Büşra Gökmen <37721917+newsteps8@users.noreply.github.com>
2020-01-21 18:12:01 +0300

    Add files via upload

---
+++ cse3063f19p1_bgokmen_fcabuk.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class cse3063f19p1_bgokmen_fcabuk {
+
+    public static void main(String[] args) throws IOException {
+        MonopolyGame monopolyGame = new MonopolyGame();
+        monopolyGame.play();
+
+    }
+
+}
+
